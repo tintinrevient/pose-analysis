973b5e584e34bc72350fa254e40a3d3469463627
Zhao Shu <zhaobenben007@googlemail.com>
2021-04-01 00:55:50 +0200

    update

--- analyze_keypoints.py
+++ analyze_keypoints.py
@@ -53,11 +53,11 @@ def calc_angle(point1, center, point2):
         dx2 = point2[0] - center[0]
         dy1 = point1[1] - center[1]
         dy2 = point2[1] - center[1]
-        m1 = np.sqrt(dx1 ** 2 + dy1 ** 2);
-        m2 = np.sqrt(dx2 ** 2 + dy2 ** 2);
+        m1 = np.sqrt(dx1 ** 2 + dy1 ** 2)
+        m2 = np.sqrt(dx2 ** 2 + dy2 ** 2)
 
         rad = np.arccos((dx1 * dx2 + dy1 * dy2) / (m1 * m2))
-        deg = rad * 180.0 / np.pi;
+        deg = rad * 180.0 / np.pi
 
         if np.isnan(rad):
             return minimum_positive_above_zero, minimum_positive_above_zero
@@ -91,11 +91,43 @@ def is_valid(keypoints):
         return True
 
 
+def clip_bbox(image, keypoints, dimension):
+
+    min_x = dimension[1]
+    max_x = 0
+    min_y = dimension[0]
+    max_y = 0
+
+    for key, value in keypoints.items():
+        x, y, score = value
+
+        if score == 0.0:
+            continue
+
+        if x < min_x and x >=0:
+            min_x = x
+        if x > max_x:
+            max_x = x
+        if y < min_y and y >=0:
+            min_y = y
+        if y > max_y:
+            max_y = y
+
+    x = int(min_x)
+    y = int(min_y)
+    w = int(max_x - min_x)
+    h = int(max_y - min_y)
+
+    image_bbox = image[y:y + h, x:x + w]
+
+    return image_bbox
+
+
 def load_keypoints(infile, output_dict={}, output_index=[], show=False):
 
     data = np.load(infile, allow_pickle='TRUE').item()
 
-    print('dimension:', data['dimension'])
+    print('input:', infile)
     print('number of people:', data['keypoints'].shape[0])
 
     person_index = 0
@@ -107,7 +139,9 @@ def load_keypoints(infile, output_dict={}, output_index=[], show=False):
     # iterate through all people
     for keypoints in data['keypoints']:
 
-        # process one person: create keypoints + rotated_keypoints
+        # process one person!!!
+
+        # create keypoints + rotated_keypoints
         keypoints = dict(zip(joint_ids, keypoints))
         rotated_keypoints = {}
 
@@ -115,59 +149,35 @@ def load_keypoints(infile, output_dict={}, output_index=[], show=False):
         if not is_valid(keypoints=keypoints):
             continue
 
-        # process one person: generate person index
+        # generate person index
         person_index += 1
         output_index.append('{}_{}'.format(index_fname, person_index))
 
-        ###############
-        # Bounding Box#
-        ###############
-        # process one person: generate bbox
-        min_x = data['dimension'][1]
-        max_x = 0
-        min_y = data['dimension'][0]
-        max_y = 0
-
-        for key, value in keypoints.items():
-            x, y, score = value
-
-            if score == 0.0:
-                continue
-
-            if x < min_x:
-                min_x = x
-            if x > max_x:
-                max_x = x
-            if y < min_y:
-                min_y = y
-            if y > max_y:
-                max_y = y
-
-        x = int(min_x)
-        y = int(min_y)
-        w = int(max_x - min_x)
-        h = int(max_y - min_y)
-        image_bbox = image[y:y + h, x:x + w]
+        ################
+        # Bounding Box #
+        ################
+        image_bbox = clip_bbox(image, keypoints, data['dimension'])
 
         person_fname = image_fname.replace('_rendered', '_' + str(person_index))
         cv2.imwrite(person_fname, image_bbox)
+        print('output', person_fname)
 
-        ###########
-        # Rotation#
-        ###########
-        # process one person: calculate the angle of rotation in rad
+        ############
+        # Rotation #
+        ############
         reference_point = keypoints['MidHip'] + [0, -100, 0]
-        rad, deg = calc_angle(point1=keypoints['Neck'], center=keypoints['MidHip'],point2=reference_point)
+        rad, deg = calc_angle(point1=keypoints['Neck'], center=keypoints['MidHip'], point2=reference_point)
 
-        # process one person: rotate the joints around keypoints['MidHip']
+        # rotate the joints around keypoints['MidHip']
+        if keypoints['Neck'][0] > keypoints['MidHip'][0]:
+            rad = -rad
+
         for key, value in keypoints.items():
-            rotated_value = rotate(value, keypoints['MidHip'], rad)
-            rotated_keypoints[key] = rotated_value
+            rotated_keypoints[key] = rotate(value, keypoints['MidHip'], rad)
 
-        #####################
-        # Angles of 3 Joints#
-        #####################
-        # process one person: calculate angles between joint triples
+        ######################
+        # Angles of 3 Joints #
+        ######################
         for index, triple in enumerate(joint_triples):
 
             point1 = keypoints.get(triple[0])
@@ -185,21 +195,39 @@ def load_keypoints(infile, output_dict={}, output_index=[], show=False):
             else:
                 output_dict[col_name].append(minimum_positive_above_zero)
 
-        #############
-        # Draw image#
-        #############
-        if show:
-            for index, pair in enumerate(joint_pairs):
-                if keypoints.get(pair[0])[2] != 0 and keypoints.get(pair[1])[2] != 0:
-                    # draw original keypoints in yellow
-                    point1 = (int(keypoints.get(pair[0])[0]), int(keypoints.get(pair[0])[1]))
-                    point2 = (int(keypoints.get(pair[1])[0]), int(keypoints.get(pair[1])[1]))
-                    cv2.line(image, point1, point2, color=(0, 255, 255), thickness=5)
+        ##################
+        # Normalize pose #
+        ##################
+        image_norm = np.zeros(data['dimension'], np.uint8)
 
-                    # draw rotated keypoints in magenta
-                    rotated_point1 = (rotated_keypoints.get(pair[0])[0], rotated_keypoints.get(pair[0])[1])
-                    rotated_point2 = (rotated_keypoints.get(pair[1])[0], rotated_keypoints.get(pair[1])[1])
-                    cv2.line(image, rotated_point1, rotated_point2, color=(255, 0, 255), thickness=5)
+        for index, pair in enumerate(joint_pairs):
+
+            point1 = keypoints.get(pair[0])
+            point2 = keypoints.get(pair[1])
+
+            rotated_point1 = rotated_keypoints.get(pair[0])
+            rotated_point2 = rotated_keypoints.get(pair[1])
+
+            if point1[2] != 0 and point2[2] != 0:
+
+                # draw original keypoints in yellow
+                point1_xy = (int(point1[0]), int(point1[1]))
+                point2_xy = (int(point2[0]), int(point2[1]))
+                cv2.line(image, point1_xy, point2_xy, color=(0, 255, 255), thickness=5)
+
+                # draw rotated keypoints in magenta
+                rotated_point1_xy = (rotated_point1[0], rotated_point1[1])
+                rotated_point2_xy = (rotated_point2[0], rotated_point2[1])
+                cv2.line(image, rotated_point1_xy, rotated_point2_xy, color=(255, 0, 255), thickness=5)
+
+                # draw rotated keypoints in normalized image
+                cv2.line(image_norm, rotated_point1_xy, rotated_point2_xy, color=(255, 0, 255), thickness=5)
+
+        image_norm_bbox = clip_bbox(image_norm, rotated_keypoints, data['dimension'])
+        image_resize = cv2.resize(image_norm_bbox, (300, 300), interpolation=cv2.INTER_AREA)
+        norm_fname = image_fname.replace('_rendered', '_' + str(person_index) + '_norm')
+        cv2.imwrite(norm_fname, image_resize)
+        print('output', norm_fname)
 
     if show:
         cv2.imshow("OpenPose 1.7.0 - Tutorial Python API", image)
